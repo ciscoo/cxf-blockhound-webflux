9b6a0a55d54fd79a4538427f7d0eb3b7f921e5fd
ciscoo <cisco21c@gmail.com>
2021-01-13 09:17:08 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,75 @@
+package com.example.demo;
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.tempuri.AddResponse;
+import org.tempuri.CalculatorSoap;
+import reactor.blockhound.BlockHound;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
+import reactor.tools.agent.ReactorDebugAgent;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    static {
+//        ReactorDebugAgent.init();
+        BlockHound.install();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> router(RouteHandler routeHandler) {
+        return RouterFunctions.route().GET("/", routeHandler).build();
+    }
+
+    @Bean
+    CalculatorSoap calculator(@Value("${calculator-address}") String url) {
+        JaxWsProxyFactoryBean factoryBean = new JaxWsProxyFactoryBean();
+        factoryBean.setServiceClass(CalculatorSoap.class);
+        factoryBean.setAddress(url);
+        return (CalculatorSoap) factoryBean.create();
+    }
+
+    @Bean
+    RouteHandler routeHandler(CalculatorSoap calculatorSoap) {
+        return new RouteHandler(calculatorSoap);
+    }
+
+    static class RouteHandler implements HandlerFunction<ServerResponse> {
+
+        private final CalculatorSoap calculator;
+
+        public RouteHandler(CalculatorSoap calculator) {
+            this.calculator = calculator;
+        }
+
+        @Override
+        public Mono<ServerResponse> handle(ServerRequest request) {
+            return Mono.<AddResponse>create(sink -> {
+                calculator.addAsync(1, 1, handler -> {
+                    try {
+                        sink.success(handler.get());
+                    } catch (Exception ex) {
+                        sink.error(ex);
+                    }
+                });
+            })
+                    .flatMap(response -> ServerResponse.ok()
+                            .bodyValue(Collections.singletonMap("result", response.getAddResult())));
+        }
+    }
+}
